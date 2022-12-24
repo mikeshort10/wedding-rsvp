d60b6a96c8285c13a934f453851cf1a5301e45ad
Mike Short <mikeshort2014@gmail.com>
2022-12-24 13:38:04 -0500

    feat: create codepipeline

--- bin/wedding-rsvp.ts
+++ bin/wedding-rsvp.ts
@@ -1,21 +1,27 @@
 #!/usr/bin/env node
-import 'source-map-support/register';
-import * as cdk from 'aws-cdk-lib';
-import { WeddingRsvpStack } from '../lib/wedding-rsvp-stack';
+import "source-map-support/register";
+import * as cdk from "aws-cdk-lib";
+import { WeddingRsvpStack } from "../lib/wedding-rsvp-stack";
+
+export type PipelineConfig = {
+  environment: "prod" | "alpha" | "devel";
+  branch: string;
+};
+
+const config: PipelineConfig = { environment: "alpha"; branch: "development" };
 
 const app = new cdk.App();
-new WeddingRsvpStack(app, 'WeddingRsvpStack', {
+
+new WeddingRsvpStack(app, "WeddingRsvpStack", {
   /* If you don't specify 'env', this stack will be environment-agnostic.
    * Account/Region-dependent features and context lookups will not work,
    * but a single synthesized template can be deployed anywhere. */
-
   /* Uncomment the next line to specialize this stack for the AWS Account
    * and Region that are implied by the current CLI configuration. */
   // env: { account: process.env.CDK_DEFAULT_ACCOUNT, region: process.env.CDK_DEFAULT_REGION },
-
   /* Uncomment the next line if you know exactly what Account and Region you
    * want to deploy the stack to. */
   // env: { account: '123456789012', region: 'us-east-1' },
-
   /* For more information, see https://docs.aws.amazon.com/cdk/latest/guide/environments.html */
+  config,
 });
--- lib/wedding-rsvp-stack.ts
+++ lib/wedding-rsvp-stack.ts
@@ -1,16 +1,33 @@
-import * as cdk from 'aws-cdk-lib';
-import { Construct } from 'constructs';
+import * as cdk from "aws-cdk-lib";
+import { Construct } from "constructs";
+import { PipelineConfig } from "../bin/wedding-rsvp";
 // import * as sqs from 'aws-cdk-lib/aws-sqs';
 
 export class WeddingRsvpStack extends cdk.Stack {
-  constructor(scope: Construct, id: string, props?: cdk.StackProps) {
+  constructor(
+    scope: Construct,
+    id: string,
+    props: cdk.StackProps & { config: PipelineConfig }
+  ) {
     super(scope, id, props);
 
-    // The code that defines your stack goes here
-
-    // example resource
-    // const queue = new sqs.Queue(this, 'WeddingRsvpQueue', {
-    //   visibilityTimeout: cdk.Duration.seconds(300)
-    // });
+    const pipeline = new cdk.pipelines.CodePipeline(
+      this,
+      `WeddingPipeline-${props?.config.environment}`,
+      {
+        synth: new cdk.pipelines.ShellStep(`Synth`, {
+          input: cdk.pipelines.CodePipelineSource.gitHub(
+            "mikeshort10/wedding-rsvp",
+            props.config.branch,
+            { trigger: cdk.aws_codepipeline_actions.GitHubTrigger.WEBHOOK }
+          ),
+          installCommands: ["npm ci"],
+          commands: ["npm synth"],
+          env: {
+            CDK_ENV: props.config.environment,
+          },
+        }),
+      }
+    );
   }
 }
