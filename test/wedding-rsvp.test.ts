43ad2bf9131c82dcab78a33167a5a6488d3e1939
Mike Short <mikeshort2014@gmail.com>
2022-12-24 13:19:50 -0500

    Initial commit

---
+++ test/wedding-rsvp.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as WeddingRsvp from '../lib/wedding-rsvp-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/wedding-rsvp-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new WeddingRsvp.WeddingRsvpStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
